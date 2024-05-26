9f60774958176dbb92b8b4376571cb012ae585b7
tonyxforce <horich.dominik@gmail.com>
2024-05-26 23:13:50 +0200

    change to all interfaces

--- py.py
+++ py.py
@@ -7,7 +7,7 @@ cap2 = cv2.VideoCapture(1)
 stream = Stream("my_camera", size=(1920, 1080), quality=100, fps=30)
 stream2 = Stream("my_camera2", size=(1920, 1080), quality=100, fps=30)
 
-server = MjpegServer("localhost", 8080)
+server = MjpegServer("0.0.0.0", 8080)
 server.add_stream(stream)
 server.add_stream(stream2)
 server.start()
